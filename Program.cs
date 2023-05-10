a3344d62a41965f6d2967f62926bcd5ef244286c
Lava1910 <113773920+Lava1910@users.noreply.github.com>
2023-05-10 07:36:36 +0700

    CRUD

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterService;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
